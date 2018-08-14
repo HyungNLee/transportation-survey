9d589bd1bc9c3622b7f16119782866111b2909c6
Hyung Lee and Meria Thomas <hyung@>
2018-08-14 10:53:45 -0700

    Make two seperate submit functions for work and fun surveys.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
-    $("#work-responses").show();
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
       $('#work-responses').append(workTransportationMode + "<br>");
+      $("#fun-survey").slideDown();
+      $('#transportation_survey').hide();
     });
-    $("#fun-responses").show();
+  });
+
+  $("form#fun-survey").submit(function(event){
+    event.preventDefault();
+    $("#work-responses").slideDown();
+    $("#fun-responses").slideDown();
+    $("#fun-survey").hide();
     $("input:checkbox[name=fun-transportation]:checked").each(function(){
       var funTransportationMode = $(this).val();
       $('#fun-responses').append(funTransportationMode + "<br>");
     });
-    $('#transportation_survey').hide();
   });
 });
